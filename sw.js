eeb6eeb69586018615d3809585f4a03ca3b47473
AgrawalHimanshi <himanshi.alg@gmail.com>
2019-12-06 18:25:17 +0530

    activate event

--- sw.js
+++ sw.js
@@ -2,3 +2,8 @@
 self.addEventListener('install', evt=>{
     console.log('service worker has been installed');
 });
+
+//activate events
+self.addEventListener('activate', evt=>{
+    console.log('service is activated');
+});
